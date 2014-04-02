b8db998ca8e7ccb19d5ca0d0d105b0071f1a6cdd
lfernandezgmail <luke.fernandez@gmail.com>
2014-04-01 21:33:31 -0600

    second commit

--- controllers/tests.js
+++ controllers/tests.js
@@ -6,21 +6,20 @@ exports.get = function(req, res, connection) {
 
 
   if (query.testname == 'css'){
-	res.end
-	(
-	'[{"testname": "CS 2350 Fernandez CSS","date": "Wed Oct 03 2012","timeElapsed": "2 minutes"},{"testname": "CS 2350 Fernandez CSS","date": "Wed Nov 03 2012","timeElapsed": "4 minutes"}]'
-	);
+  res.end
+  (
+  '[{"testname": "CS 2350 Fernandez CSS","date": "Wed Oct 03 2012","quesCorrect": [1,2,3,4,5]},{"testname": "CS 2350 Fernandez CSS","date": "Wed Nov 03 2012","quesCorrect": [1,2]}]'
+  );
   }
   else if (query.testname == 'xhtml'){
 
-  	res.end
-  	(
-  	'[{    "testname": "CS 2350 Fernandez XHTML",    "date": "Thur Oct 07 2012",    "timeElapsed": "6 minutes"    },{    "testname": "CS 2350 Fernandez XHTML",    "date": "Sun Oct 09 2012",    "timeElapsed": "7 minutes"    },    {    "testname": "CS 2350 Fernandez XHTML",    "date": "Fri Dec 03 2012",    "timeElapsed": "8 minutes"    }]'	
-  	);
+    res.end
+    (
+    '[{    "testname": "CS 2350 Fernandez XHTML",    "date": "Thur Oct 07 2012",    "quesCorrect": [4,5]    },{    "testname": "CS 2350 Fernandez XHTML",    "date": "Sun Oct 09 2012",    "quesCorrect": [1,2,3,4]    },    {    "testname": "CS 2350 Fernandez XHTML",    "date": "Fri Dec 03 2012",    "quesCorrect": [1,3,4]    }]' 
+    );
 
   }
   else{
-  	res.end( JSON.stringify('You did not enter css or xhtml')  );
+    res.end( JSON.stringify('You did not enter css or xhtml')  );
   }
 }
-
